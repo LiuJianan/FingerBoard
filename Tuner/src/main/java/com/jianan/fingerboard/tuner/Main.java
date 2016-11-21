fc676d0b23c5d0c7a255e5aaff5d3c9854f54395
jianan.liu <jianan.liu@qunar.com>
2016-11-22 01:22:14 +0800

    http

--- Tuner/src/main/java/com/jianan/fingerboard/tuner/Main.java
+++
@@ -1,8 +0,0 @@
-package com.jianan.fingerboard.tuner;
-
-/**
- * @author by jianan.liu on 16/11/20.
- */
-public class Main {
-
-}
--- Tuner/src/main/java/com/jianan/fingerboard/tuner/http/AsyncHttpUtil.java
+++ Tuner/src/main/java/com/jianan/fingerboard/tuner/http/AsyncHttpUtil.java
@@ -3,20 +3,25 @@ package com.jianan.fingerboard.tuner.http;
 import java.io.IOException;
 import java.util.concurrent.Future;
 
-import com.jianan.fingerboard.tuner.json.JsonUtil;
+import com.google.common.collect.Maps;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.concurrent.FutureCallback;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.http.impl.nio.client.HttpAsyncClients;
 import org.apache.http.impl.nio.conn.PoolingNHttpClientConnectionManager;
 import org.apache.http.impl.nio.reactor.DefaultConnectingIOReactor;
 import org.apache.http.nio.reactor.ConnectingIOReactor;
+import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.jianan.fingerboard.tuner.json.JsonUtil;
 
 /**
  * Async Http client
@@ -30,6 +35,9 @@ public class AsyncHttpUtil {
     private static final int CONNECT_TIMEOUT = 30000;
     private static final int MAX_TOTAL = 200;
     private static final int MAX_HOST = 20;
+    private static final String APPLICATION_JSON = "application/json";
+    private static final String TEXT_JSON = "text/json";
+    private static final String UTF = "text/json";
 
     private static RequestConfig config;
     private static CloseableHttpAsyncClient httpclient;
@@ -67,7 +75,7 @@ public class AsyncHttpUtil {
     }
 
     public static String get(final String url) {
-        final HttpGet httpGet = new HttpGet(url);
+        HttpGet httpGet = new HttpGet(url);
         Future<HttpResponse> httpResponseFuture = httpclient.execute(httpGet, new FutureCallback<HttpResponse>() {
             public void completed(HttpResponse httpResponse) {
                 logger.info("ok url: {}", url);
@@ -81,9 +89,36 @@ public class AsyncHttpUtil {
                 logger.warn("cancel url: {}", url);
             }
         });
-        System.out.println(pool.getTotalStats().toString());
-        System.out.println(JsonUtil.serialize(pool.getRoutes()));
         return parse(httpResponseFuture);
+    }
+
+    public static String post(final String url, Object object) {
+        try {
+            HttpPost httpPost = new HttpPost(url);
+            httpPost.setHeader(HTTP.CONTENT_TYPE, APPLICATION_JSON);
+
+            String json = JsonUtil.serialize(object);
+            StringEntity entity = new StringEntity(json, "utf-8");
+            entity.setContentType(TEXT_JSON);
+            httpPost.setEntity(entity);
+            Future<HttpResponse> httpResponseFuture = httpclient.execute(httpPost, new FutureCallback<HttpResponse>() {
+                public void completed(HttpResponse httpResponse) {
+                    logger.info("ok url: {}", url);
+                }
+
+                public void failed(Exception e) {
+                    logger.error("fail url: {}", url);
+                }
+
+                public void cancelled() {
+                    logger.warn("cancel url: {}", url);
+                }
+            });
+            return parse(httpResponseFuture);
+        } catch (Exception e) {
+            logger.error("post error for url: {} object: {}", url, object);
+            return null;
+        }
     }
 
     private static String parse(Future<HttpResponse> httpResponseFuture) {
