ae414d66b203cefe71bd17befa75cf5f6bb40df0
jianan.liu <jianan.liu@qunar.com>
2017-02-13 00:51:13 +0800

    master

---
+++ Resonate/src/main/java/com/jianan/fingerborad/resonate/config/client/AppConfigClient.java
@@ -0,0 +1,41 @@
+package com.jianan.fingerborad.resonate.config.client;
+
+import com.jianan.fingerborad.resonate.config.server.AppConfigServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author by jianan.liu on 17/2/12.
+ */
+@SpringBootApplication
+@RestController
+@Configuration
+@EnableAutoConfiguration
+@RefreshScope
+public class AppConfigClient {
+    @Value("${config.test.name:world}")
+    String bar;
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello " + bar + "!";
+    }
+
+    @Autowired
+    void setEnviroment(Environment env) {
+        System.out.println("my-config.appName from env: "
+                + env.getProperty("config.test.name"));
+    }
+    public static void main(String[] args) {
+        Class[] classes = new Class[] {AppConfigServer.class, AppConfigClient.class};
+        SpringApplication.run(classes, args);
+    }
+}
