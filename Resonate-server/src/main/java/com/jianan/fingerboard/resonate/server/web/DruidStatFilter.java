06f7454d98f32f1c8ff9f2b8fff5077010350c31
jianan.liu <jianan.liu@qunar.com>
2017-04-11 20:46:26 +0800

    druid

--- Resonate-server/src/main/java/com/jianan/fingerboard/resonate/server/web/DruidStatFilter.java
+++ Resonate-server/src/main/java/com/jianan/fingerboard/resonate/server/web/DruidStatFilter.java
@@ -6,11 +6,7 @@ import javax.servlet.annotation.WebInitParam;
 import com.alibaba.druid.support.http.WebStatFilter;
 
 /**
- * Druid的StatFilter
- *
- * @author   单红宇(365384722)
- * @myblog  http://blog.csdn.net/catoop/
- * @create    2016年3月17日
+ * @author liujianan
  */
 @WebFilter(filterName="druidWebStatFilter",urlPatterns="/*",
         initParams={
--- Resonate-server/src/main/java/com/jianan/fingerboard/resonate/server/web/DruidStatViewServlet.java
+++ Resonate-server/src/main/java/com/jianan/fingerboard/resonate/server/web/DruidStatViewServlet.java
@@ -6,11 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import com.alibaba.druid.support.http.StatViewServlet;
 
 /**
- * StatViewServlet
- *
- * @author 单红宇(365384722)
- * @myblog http://blog.csdn.net/catoop/
- * @create 2016年3月17日
+ * @author liujianan
  */
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = "/druid/*",
